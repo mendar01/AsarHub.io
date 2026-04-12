c310cb4f83af0639e106156b7b2dcb7bd82e4f56
mendar01 <Darmen050409@gmail.com>
2026-04-12 11:45:49 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3RFMInivmbCmiFEAYwDb0LsXlOoZkank",
+  authDomain: "asar-hub-2888f.firebaseapp.com",
+  projectId: "asar-hub-2888f",
+  storageBucket: "asar-hub-2888f.firebasestorage.app",
+  messagingSenderId: "331447109133",
+  appId: "1:331447109133:web:624ffac28fa5828982f744",
+  measurementId: "G-XRGZ667W2Q"
+};
